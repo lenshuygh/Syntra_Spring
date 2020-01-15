97802981b984c3ef8584758d7f660ddb26ba9dcf
lens.huygh@gmail.com <ath3gat35>
2020-01-15 19:45:33 +0100

    ex3p423 scopes

---
+++ src/main/java/be/lens/spring/baeldung_tutorial/contoller/CounterController.java
@@ -0,0 +1,33 @@
+package be.lens.spring.baeldung_tutorial.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Controller
+@SessionScope
+public class CounterController {
+    private int count = 0;
+
+    @PostConstruct
+    public void init(){
+        System.out.println("CounterController : init()");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("CounterController : destroy()");
+    }
+
+    @RequestMapping("counter")
+    protected ModelAndView handle(){
+        return new ModelAndView("counterView","count",++count);
+    }
+
+}
