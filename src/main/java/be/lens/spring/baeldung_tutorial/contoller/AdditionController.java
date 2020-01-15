ffc6f5090e7ee6d4cf416819daf5d2cb866b9769
lens.huygh@gmail.com <ath3gat35>
2020-01-15 21:19:51 +0100

    ex6p437 use a GET for init empty page use a POST to calculate en display result

--- src/main/java/be/lens/spring/baeldung_tutorial/contoller/AdditionController.java
+++ src/main/java/be/lens/spring/baeldung_tutorial/contoller/AdditionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/addition")
 public class AdditionController {
-    /*@RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public ModelAndView addNumbers(@RequestParam("first_number") int num1,@RequestParam("second_number") int num2){
         return new ModelAndView("additionPage","additionresult",num1 + num2);
     }
@@ -17,9 +17,9 @@ public class AdditionController {
     @RequestMapping(method = RequestMethod.GET)
     public String displayForm(){
         return "additionPage";
-    }*/
+    }
 
-    @RequestMapping(method = RequestMethod.GET)
+    /*@RequestMapping(method = RequestMethod.GET)
     public ModelAndView addAndOrDisplayForm(@RequestParam(value="first_number",required = false) String num1,@RequestParam(value="second_number",required = false) String num2){
         if((null == num1 || null == num2) || (("").equals(num1) || (("").equals(num2)))){
             return new ModelAndView("additionPage");
@@ -28,5 +28,5 @@ public class AdditionController {
             return new ModelAndView("additionPage","additionresult",rez);
         }
 
-    }
+    }*/
 }
