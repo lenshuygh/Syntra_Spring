820f06bda588a6d97429ed197509ef8ac632cb1f
lens.huygh@gmail.com <ath3gat35>
2020-01-15 21:17:24 +0100

    ex6p437 combine in one GET method

---
+++ src/main/java/be/lens/spring/baeldung_tutorial/contoller/AdditionController.java
@@ -0,0 +1,32 @@
+package be.lens.spring.baeldung_tutorial.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/addition")
+public class AdditionController {
+    /*@RequestMapping(method = RequestMethod.POST)
+    public ModelAndView addNumbers(@RequestParam("first_number") int num1,@RequestParam("second_number") int num2){
+        return new ModelAndView("additionPage","additionresult",num1 + num2);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String displayForm(){
+        return "additionPage";
+    }*/
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView addAndOrDisplayForm(@RequestParam(value="first_number",required = false) String num1,@RequestParam(value="second_number",required = false) String num2){
+        if((null == num1 || null == num2) || (("").equals(num1) || (("").equals(num2)))){
+            return new ModelAndView("additionPage");
+        }else{
+            int rez = Integer.parseInt(num1) + Integer.parseInt(num2);
+            return new ModelAndView("additionPage","additionresult",rez);
+        }
+
+    }
+}
