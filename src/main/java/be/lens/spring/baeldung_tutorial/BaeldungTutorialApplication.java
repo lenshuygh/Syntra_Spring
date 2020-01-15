b1e3bcf87c6d3dcd202f645b91f442c0e1af7f5a
lens.huygh@gmail.com <ath3gat35>
2020-01-15 20:01:43 +0100

    ex4p425 i18n

--- src/main/java/be/lens/spring/baeldung_tutorial/BaeldungTutorialApplication.java
+++ src/main/java/be/lens/spring/baeldung_tutorial/BaeldungTutorialApplication.java
@@ -2,12 +2,25 @@ package be.lens.spring.baeldung_tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BaeldungTutorialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BaeldungTutorialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BaeldungTutorialApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer configurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/welcome").setViewName("welcome");
+            }
+        };
+    }
 
 }
