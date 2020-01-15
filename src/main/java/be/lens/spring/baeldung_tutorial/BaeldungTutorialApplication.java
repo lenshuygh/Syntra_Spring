3e7adb1baf2ce68efd847e6033690c0943c62cc8
lens.huygh@gmail.com <ath3gat35>
2020-01-15 19:08:36 +0100

    init commit, tutorial finished

---
+++ src/main/java/be/lens/spring/baeldung_tutorial/BaeldungTutorialApplication.java
@@ -0,0 +1,13 @@
+package be.lens.spring.baeldung_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeldungTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/lens/spring/baeldung_tutorial/BaeldungTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package be.lens.spring.baeldung_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
