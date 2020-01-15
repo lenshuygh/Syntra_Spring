7052440213d31ba91fff7bc02bf8778aa4ebc68c
lens.huygh@gmail.com <ath3gat35>
2020-01-15 19:32:33 +0100

    add PostConstruct and PreDestroy methods to bean

--- src/main/java/be/lens/spring/baeldung_tutorial/service/HelloService.java
+++ src/main/java/be/lens/spring/baeldung_tutorial/service/HelloService.java
@@ -2,10 +2,23 @@ package be.lens.spring.baeldung_tutorial.service;
 
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Service("helloService")
 public class HelloService implements Hello {
     @Override
     public String sayHello() {
         return "Hello from helloService";
+    }
+
+    @PostConstruct
+    private void afterBean(){
+        System.out.println("POSTCONSTRUCT");
+    }
+
+    @PreDestroy
+    private void beforeDestroy(){
+        System.out.println("PREDESTROY");
     }
 }
