2785b08831891c2ab89fdd6bc5b2e7d6688b8403
lens.huygh@gmail.com <ath3gat35>
2020-01-20 20:43:03 +0100

    ex8p444

---
+++ src/main/java/be/lens/spring/baeldung_tutorial/contoller/AddressController.java
@@ -0,0 +1,7 @@
+package be.lens.spring.baeldung_tutorial.contoller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AddressController {
+}
---
+++ src/main/java/be/lens/spring/baeldung_tutorial/model/Address.java
@@ -0,0 +1,67 @@
+package be.lens.spring.baeldung_tutorial.model;
+
+public class Address {
+    private String name;
+    private String surName;
+    private String street;
+    private int number;
+    private int zipCode;
+    private String city;
+    private String country;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
