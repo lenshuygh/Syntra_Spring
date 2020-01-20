df584a89375ba6c832abc7e0e356389659e8a269
lens.huygh@gmail.com <ath3gat35>
2020-01-20 21:16:39 +0100

    ex8p444 basic implementation

--- src/main/java/be/lens/spring/baeldung_tutorial/contoller/AddressController.java
+++ src/main/java/be/lens/spring/baeldung_tutorial/contoller/AddressController.java
@@ -1,7 +1,21 @@
 package be.lens.spring.baeldung_tutorial.contoller;
 
+import be.lens.spring.baeldung_tutorial.model.Address;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/address")
 public class AddressController {
+    @GetMapping
+    public String displayForm(){
+        return "address";
+    }
+
+    @PostMapping
+    public String processAddress(Address address){
+        return "addressDetail";
+    }
 }
