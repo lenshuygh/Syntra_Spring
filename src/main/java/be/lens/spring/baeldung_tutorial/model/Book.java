024bcbdc0ede70bec424f9e060d4970987eb5e3b
lens.huygh@gmail.com <ath3gat35>
2020-01-20 21:40:23 +0100

    ex10p446 add lombok dependency add book bean

---
+++ src/main/java/be/lens/spring/baeldung_tutorial/model/Book.java
@@ -0,0 +1,40 @@
+package be.lens.spring.baeldung_tutorial.model;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private float price;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
